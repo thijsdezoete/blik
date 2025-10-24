5fdccda456dceeb64067be84a979e3d16716f2ed
Thijs de Zoete <thijsdezoete@gmail.com>
2025-10-24 21:51:41 +0200

    Make nginx also listen to these ports then

--- docker-entrypoint-nginx.sh
+++ docker-entrypoint-nginx.sh
@@ -2,11 +2,12 @@
 set -e
 
 echo "Configuring nginx with environment variables..."
+echo "NGINX_PORT: ${NGINX_PORT:-80}"
 echo "PORT: ${PORT:-8000}"
 echo "LANDING_PORT: ${LANDING_PORT:-8001}"
 
 # Substitute environment variables in nginx config template
-envsubst '${PORT} ${LANDING_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${NGINX_PORT} ${PORT} ${LANDING_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "Nginx configuration ready"
 echo ""
